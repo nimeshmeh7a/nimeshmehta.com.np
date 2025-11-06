a937ba1ef2a4cd1b88b8889e884cac0dcf8c2621
NIMESH <nimeshmeh7a@NIMESHs-MacBook-Air.local>
2025-11-06 14:39:42 +0545

    Update configuration for custom domain nimeshmehta.com.np

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nimeshmehta.com.np/', // GitHub Pages base path
+  base: '/', // Custom domain base path
 })
